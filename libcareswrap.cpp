8b9151f09e00ed83055b851890b58fbe9897021e
Simone Basso <bassosimone@gmail.com>
2018-04-19 17:57:23 +0200

    Add TODO comment

--- libcareswrap.cpp
+++ libcareswrap.cpp
@@ -68,6 +68,7 @@ using namespace measurement_kit::libcareswrap;
   } while (0)
 
 static bool now() noexcept {
+  // TODO(bassosimone): this does not seem to work as expected initially.
   static std::chrono::time_point<std::chrono::steady_clock> t0;
   std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - t0;
   return elapsed.count();
